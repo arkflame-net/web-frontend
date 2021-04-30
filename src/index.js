2ec45c44ae6acb56507cf09e916ffc5ac5b1105a
Gerardo Wacker <ge@rar.vg>
2021-04-30 10:35:47 -0300

    Added normalize.css to the project

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 // Libraries
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
 // Providers and components
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 
 // Main app component
-import App from './App';
+import App from "./App";
 
 // Services
-import { validate } from './services/authService';
+import { validate } from "./services/authService";
 
 // Redux
-import { Provider } from 'react-redux';
-import { sessionService } from 'redux-react-session';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import { sessionService } from "redux-react-session";
+import store from "./redux/store";
+
+// Normalize
+import "normalize.css";
 
 sessionService.initSessionService(store, {
   validateSession: async (session) => {
     const valid = await validate(session.token);
     return valid;
-  }
+  },
 });
 
 ReactDOM.render(
@@ -27,6 +30,6 @@ ReactDOM.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </ChakraProvider>
-  , document.getElementById('root')
+  </ChakraProvider>,
+  document.getElementById("root")
 );
