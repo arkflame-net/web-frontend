a8499e43973939078768da718f7e3cc37984cc39
Gerardo Wacker <ge@rar.vg>
2021-04-30 21:58:39 -0300

    Removed empty button in Home (I was going insane)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,30 +7,37 @@ import SummaryBigDoubles from "../components/layout/SummaryBigDoubles";
 
 import Button from "../components/gui/button";
 
-export default function Home (props) {
-    return (
-        <div>
-            <SummaryFullWide title="ArkFlame Network" subtitle="¡Bienvenido a nuestro Servidor! Disfruta en cualquiera de tus versiones favoritas (1.7.x - 1.17.x)">
-                <Button width="100">play.arkflame.com</Button>
-            </SummaryFullWide>
+export default function Home(props) {
+  return (
+    <div>
+      <SummaryFullWide
+        title="ArkFlame Network"
+        subtitle="¡Bienvenido a nuestro Servidor! Disfruta en cualquiera de tus versiones favoritas (1.7.x - 1.17.x)"
+      />
 
-            <SummaryDoubles left={{ title: "Left", subtitle: "LOL" }} right={{ title:"Right", subtitle: "LOL but right side"}} />
-            <SummaryTrio 
-                left={{ title: "Left", subtitle: "LOL" }} 
-                middle={{ title:"I'm centered", subtitle: "Yup"}} 
-                right={{ title:"Right", subtitle: "LOL but right side"}} 
-            />
-            <SummaryDoublesAndBig
-                right={{ title: "Right", subtitle: "LOL" }} 
-                up={{ title:"UP", subtitle: "Yup"}} 
-                down={{ title:"DOWN", subtitle: "LOL but bottom side"}} 
-            />
-            <SummaryBigAndDoubles
-                left={{ title: "Left", subtitle: "LOL" }} 
-                up={{ title:"UP", subtitle: "Yup"}} 
-                down={{ title:"DOWN", subtitle: "LOL but bottom side"}} 
-            />
-            <SummaryBigDoubles left={{ title: "Left", subtitle: "LOL" }} right={{ title:"Right", subtitle: "LOL but right side"}}/>
-        </div>
-    )
-} 
+      <SummaryDoubles
+        left={{ title: "Left", subtitle: "LOL" }}
+        right={{ title: "Right", subtitle: "LOL but right side" }}
+      />
+      <SummaryTrio
+        left={{ title: "Left", subtitle: "LOL" }}
+        middle={{ title: "I'm centered", subtitle: "Yup" }}
+        right={{ title: "Right", subtitle: "LOL but right side" }}
+      />
+      <SummaryDoublesAndBig
+        right={{ title: "Right", subtitle: "LOL" }}
+        up={{ title: "UP", subtitle: "Yup" }}
+        down={{ title: "DOWN", subtitle: "LOL but bottom side" }}
+      />
+      <SummaryBigAndDoubles
+        left={{ title: "Left", subtitle: "LOL" }}
+        up={{ title: "UP", subtitle: "Yup" }}
+        down={{ title: "DOWN", subtitle: "LOL but bottom side" }}
+      />
+      <SummaryBigDoubles
+        left={{ title: "Left", subtitle: "LOL" }}
+        right={{ title: "Right", subtitle: "LOL but right side" }}
+      />
+    </div>
+  );
+}
