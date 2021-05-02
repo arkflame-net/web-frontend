edd1b8e9f924c2a41d70258c96bc02d8b6ad864c
Gerardo Wacker <ge@rar.vg>
2021-05-02 02:13:33 -0300

    Added NewsPost page

---
+++ src/pages/NewsPost.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Post from '../components/layout/Post';
+import marked from 'marked'
+import parseMD from 'parse-md'
+import Link from 'react-dom'
+
+const importAll = (r) => r.keys().map(r);
+const postFiles = importAll(require.context('../content/', true, /\.md$/)).sort().reverse();
+
+export default class NewsPost extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            post: null
+        }
+    } 
+
+    componentDidMount() {
+        const path = window.location.pathname;
+        const shrug = path.split("/")[path.split("/").length - 1];
+
+        const post = postFiles.filter(file => file.default.split('/')[3].split('.')[0] === shrug)
+
+        if(post.length > 0) {
+            fetch(post[0].default).then(r => r.text().then(m => {
+                this.setState({
+                    post: m
+                })
+            }))
+        }
+    }
+
+    render() {
+        if(this.state.post)
+            return (
+                <div>
+                    <Post content={this.state.post} />
+                </div>
+            )
+        else return <div></div>
+    }
+}
