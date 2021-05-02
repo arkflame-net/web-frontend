423da53a0cd1f9e4dab9d3071bdf7c137ce61156
Gerardo Wacker <ge@rar.vg>
2021-05-02 15:06:33 -0300

    Added check for empty news post

--- src/pages/NewsPost.jsx
+++ src/pages/NewsPost.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
-import Post from '../components/layout/Post';
+import React from "react";
+import Post from "../components/layout/Post";
 
 const importAll = (r) => r.keys().map(r);
-const postFiles = importAll(require.context('../content/', true, /\.md$/)).sort().reverse();
+const postFiles = importAll(require.context("../content/", true, /\.md$/))
+  .sort()
+  .reverse();
 
 export default class NewsPost extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
+    this.state = {
+      post: null,
+    };
+  }
 
-        this.state = {
-            post: null
-        }
-    } 
+  componentDidMount() {
+    const path = window.location.pathname;
+    const shrug = path.split("/")[path.split("/").length - 1];
 
-    componentDidMount() {
-        const path = window.location.pathname;
-        const shrug = path.split("/")[path.split("/").length - 1];
+    const post = postFiles.filter(
+      (file) => file.default.split("/")[3].split(".")[0] === shrug
+    );
 
-        const post = postFiles.filter(file => file.default.split('/')[3].split('.')[0] === shrug)
+    post.length > 0
+      ? fetch(post[0].default).then((r) =>
+          r.text().then((m) => this.setState({ post: m }))
+        )
+      : (window.location.pathname = "/");
+  }
 
-        if(post.length > 0) {
-            fetch(post[0].default).then(r => r.text().then(m => {
-                this.setState({
-                    post: m
-                })
-            }))
-        }
-    }
-
-    render() {
-        if(this.state.post)
-            return (
-                <div>
-                    <Post content={this.state.post} />
-                </div>
-            )
-        else return <div></div>
-    }
+  render() {
+    if (this.state.post)
+      return (
+        <div>
+          <Post content={this.state.post} />
+        </div>
+      );
+    else return <div></div>;
+  }
 }
+
