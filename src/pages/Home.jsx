52adc77dbc4264ef349eb1a470b2c4af2fca9f8d
Gerardo Wacker <ge@rar.vg>
2021-05-03 00:26:34 -0300

    Added news as a container called Noticias

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import SummaryFullWide from '../components/layout/SummaryFullWide'
 import parseMD from 'parse-md'
+import Container from '../components/layout/Container';
+import SummaryFullMedium from '../components/layout/SummaryFullMedium';
 
 const importAll = (r) => r.keys().map(r);
 const postFiles = importAll(require.context('../content/', true, /\.md$/)).sort().reverse();
@@ -28,17 +30,22 @@ export default class Home extends React.Component {
     if(this.state)
       return (
         <div>
-          {
-              this.state.posts.map((post, key) => (
-                <Link to={"/news/" + parseMD(post).metadata.id}>
-                  <SummaryFullWide
-                    title={parseMD(post).metadata.title}
-                    subtitle={`Escrito por ${parseMD(post).metadata.author}`}
-                    image={parseMD(post).metadata.banner}
-                  />
-                </Link>
-              ))
-          }
+          <Container
+            name={{text: "Noticias", link: "#"}}
+            header={[" Inicio > ", <a href="#">{"Noticias"}</a>]}
+          >
+            {
+                this.state.posts.map((post, key) => (
+                  <Link to={"/news/" + parseMD(post).metadata.id}>
+                    <SummaryFullWide
+                      title={parseMD(post).metadata.title}
+                      subtitle={`Escrito por ${parseMD(post).metadata.author}`}
+                      image={parseMD(post).metadata.banner}
+                    />
+                  </Link>
+                ))
+            }
+          </Container>
         </div>
       )
     else return <div style={{color: "white"}}>Cargando...</div>
