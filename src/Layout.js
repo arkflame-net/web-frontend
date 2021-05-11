1a203cd484f989712796238b81b860e8e247e06d
Gerardo Wacker <ge@rar.vg>
2021-05-11 12:35:11 -0300

    Added props in Sidebar

--- src/Layout.js
+++ src/Layout.js
@@ -1,26 +1,27 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 import Hero from "./components/layout/hero";
 import Navbar from "./components/layout/navbar";
 import Sidebar from "./components/layout/sidebar";
 
 import "./style.sass";
-import Footer from './components/layout/Footer';
+import Footer from "./components/layout/Footer";
 
-export default function Layout (props) {
-    return (
-        <BrowserRouter>
-            <Hero />
-            <div className="view-model">
-                <Navbar {...props} />
-                <div className="container">
-                    <div className="content">
-                        <Router {...props} />
-                    </div>
-                    <Sidebar />
-                </div>
-            </div>
-            <Footer />
-        </BrowserRouter>
-    )
+export default function Layout(props) {
+  return (
+    <BrowserRouter>
+      <Hero />
+      <div className="view-model">
+        <Navbar {...props} />
+        <div className="container">
+          <div className="content">
+            <Router {...props} />
+          </div>
+          <Sidebar {...props} />
+        </div>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
 }
+
