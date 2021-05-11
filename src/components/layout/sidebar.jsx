39b4efbd99376ba57145eba149d5f97acb4e188e
Gerardo Wacker <ge@rar.vg>
2021-05-11 12:35:49 -0300

    Added SidebarUserElement into Sidebar

--- src/components/layout/sidebar.jsx
+++ src/components/layout/sidebar.jsx
@@ -1,15 +1,22 @@
 import styles from "./sidebar.module.sass";
+import SidebarUserElement from "./SidebarUserElement";
 
-export default function Sidebar () {
-    return (
-        <div className={styles["sidebar"]}>
-          <div>
-            <iframe src="https://discord.com/widget?id=287770195441876992&theme=dark" 
-              allowtransparency="true" frameborder="0" width="260" height="240"
-              title="Discord"
-              sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
-            </iframe>
-          </div>          
-        </div>
-    )
+export default function Sidebar({ user }) {
+  return (
+    <div className={styles["sidebar"]}>
+      {user != null ? <SidebarUserElement user={user} /> : ""}
+      <div>
+        <iframe
+          src="https://discord.com/widget?id=287770195441876992&theme=dark"
+          allowtransparency="true"
+          frameborder="0"
+          width="260"
+          height="240"
+          title="Discord"
+          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+        ></iframe>
+      </div>
+    </div>
+  );
 }
+
