26eb00df38174ce33b2406211ad1e25a61cb340a
Gerardo Wacker <ge@rar.vg>
2021-05-12 10:20:15 -0300

    Added Profile into Router

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 // Providers and components
 import { Fragment } from "react";
-import Route from './components/utils/SmartRoute';
+import Route from "./components/utils/SmartRoute";
 
 // Pages
 import Basket from "./pages/Basket.jsx";
@@ -8,65 +8,32 @@ import Category from "./pages/Category.jsx";
 import Community from "./pages/Community.jsx";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login";
-import NewsPost from './pages/NewsPost.jsx'
+import NewsPost from "./pages/NewsPost.jsx";
+import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import Rules from "./pages/Rules";
 
-export default function Router (props) {
-    return (
-        <Fragment>
-            <Route
-                {...props}
-                path="/"
-                exact
-                Component={Home}
-            />
+export default function Router(props) {
+  return (
+    <Fragment>
+      <Route {...props} path="/" exact Component={Home} />
 
-            <Route
-                {...props}
-                path="/community"
-                exact
-                Component={Community}
-            />
+      <Route {...props} path="/community" exact Component={Community} />
 
-            <Route
-                {...props}
-                path="/store/basket"
-                Component={Basket}
-            />
+      <Route {...props} path="/store/basket" Component={Basket} />
 
-            <Route
-                {...props}
-                path="/store/category"
-                Component={Category}
-            />
+      <Route {...props} path="/store/category" Component={Category} />
 
-            <Route
-                {...props}
-                path="/news"
-                Component={NewsPost}
-            />
-            
-            <Route
-                {...props}
-                exact
-                path="/rules"
-                Component={Rules}
-            />
+      <Route {...props} path="/news" Component={NewsPost} />
 
-            <Route
-                {...props}
-                exact
-                path="/login"
-                Component={Login}
-            />
+      <Route {...props} exact path="/rules" Component={Rules} />
 
-            <Route
-                {...props}
-                exact
-                path="/register"
-                Component={Register}
-            />
-        </Fragment>
-    )
+      <Route {...props} exact path="/login" Component={Login} />
+
+      <Route {...props} exact path="/register" Component={Register} />
+
+      <Route {...props} path="/user" Component={Profile} />
+    </Fragment>
+  );
 }
+
