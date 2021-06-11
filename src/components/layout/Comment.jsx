97672ea974a14ce70afdff4e0e823861916f74da
Gerardo Wacker <ge@rar.vg>
2021-06-10 21:05:06 -0300

    Updated Comment component

--- src/components/layout/Comment.jsx
+++ src/components/layout/Comment.jsx
@@ -6,9 +6,9 @@ export default class Comment extends React.Component {
         const { author, message } = this.props
         if(author && message) return (
             <div className="comment">
-                <img className="picture" src={author.picture} />
+                <img draggable="false" className="picture" src={author.picture} />
                 <div className="normal">
-                    <span style={{fontWeight: "bold"}}>{author.name}</span><br />
+                    <span style={{userSelect: "none", fontWeight: "bold"}}>{author.name}</span><br />
                     <span>{message}</span>
                 </div>
             </div>
