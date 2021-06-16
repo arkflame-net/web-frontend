4f940aba1ec2f76339c82adf328ee0ae9f5ff04d
Gerardo Wacker <ge@rar.vg>
2021-06-16 10:17:24 -0300

    Added functional Basket list

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -10,34 +10,41 @@ import { FiTrash2 } from 'react-icons/fi'
 import Container from '../components/layout/Container'
 import { Link } from 'react-router-dom'
 import './Basket.css'
+import { fetchAllProductsQuery } from "../graphql";
+import { QueryRenderer } from "react-relay";
 
 var basketTotal;
-var updateBasketTotal;
 
 export class BasketItem extends React.Component {
     render() {
-        const { basket, item, parent } = this.props
+        const { basket, item, parent, metadata } = this.props
+        console.log(metadata)
         return (
             <Flex style={{borderBottom: "3px solid #313742"}} direction={{ base: "row", sm: "column" }} bg="#424853" color="#fff">
-                <SimpleGrid spacingY={3} columns={{ base: 1, sm: 6 }} w="full" py={3} px={10} fontWeight="hairline">
-                    <span>{item.name}</span>
-                    <chakra.span textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">{item.price}</chakra.span>
-                    <chakra.span textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">{item.category}</chakra.span>
-                    <chakra.span textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">{basket.getItemAmount(item.id)}</chakra.span>
-                    <chakra.span textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">{basket.getItemAmount(item.id) * item.price}</chakra.span>
+                <SimpleGrid spacingY={3} columns={{ base: 1, sm: 5 }} w="full" py={3} px={10} fontWeight="hairline">
                     <span>
                         <Button onClick={(e) => {e.preventDefault(); parent.removeItem(item.id)}}
                         variant="solid" style={{ "display": "inline-block", "margin": "1px 5px" }} colorScheme="red" size="sm">
                             <FiTrash2 />
                         </Button>
                     </span>
+                    <span>{metadata.name}</span>
+                    <chakra.span textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">${metadata.price}</chakra.span>
+                    <chakra.span textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">{basket.getItemAmount(item.id)}</chakra.span>
+                    <chakra.span textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">${basket.getItemAmount(item.id) * metadata.price}</chakra.span>
                 </SimpleGrid>
             </Flex>
-        );
+        )
     }
 }
 
 export function BasketFooter(props) {
+    let subtotal = 0
+    props.basket.getItems().forEach(basketItem => {
+        let itemMetadata = props.metadata.filter(storeItem => storeItem._id == basketItem.id)[0]
+        subtotal = subtotal + (itemMetadata.price * basketItem.amount)
+    })
+
     return (<SimpleGrid
         spacingY={3}
         columns={{ base: 1, sm: 1 }}
@@ -52,7 +59,7 @@ export function BasketFooter(props) {
         borderRadius="0px 0px 10px 10px"
         justifyContent="space-between"
     >
-        <span style={{textAlign: "right", fontWeight: "bold", fontSize: "18px"}}>Subtotal: ${props.subtotal}</span>
+        <span style={{textAlign: "right", fontWeight: "bold", fontSize: "18px"}}>Subtotal: ${subtotal}</span>
         <Link to="/store" style={{width: "100%"}}><Button style={{background: "#3edb00", color: "#000", width: "100%"}}>Seguir comprando</Button></Link>
         <Link to="/store/checkout" style={{width: "100%"}}><Button disabled={props.basket.getItems().length <= 0} style={{background: "rgb(255, 196, 77)", color: "#000", width: "100%"}}>Terminar compra</Button></Link>
     </SimpleGrid>
@@ -62,7 +69,7 @@ export function BasketFooter(props) {
 export function BasketHeader(props) {
     return (<SimpleGrid
         spacingY={3}
-        columns={{ base: 1, sm: 6 }}
+        columns={{ base: 1, sm: 5 }}
         w={{ base: 100, sm: "full" }}
         textTransform="uppercase"
         bg="#313742"
@@ -73,12 +80,11 @@ export function BasketHeader(props) {
         fontWeight="hairline"
         borderRadius="10px 10px 0px 0px"
     >
+        <span></span>
         <span>Nombre</span>
         <span>Precio</span>
-        <span>Categoría</span>
         <span>Cantidad</span>
         <span>Total</span>
-        <span>Actions</span>
     </SimpleGrid>
     );
 }
@@ -92,7 +98,7 @@ export default class Basket extends React.Component {
 
     componentDidMount() {
         const {basket} = this.props
-
+        
         if(basket) this.setState({items: basket.getItems()})
     }
 
@@ -102,19 +108,15 @@ export default class Basket extends React.Component {
         this.setState({items: basket.getItems()})
     }
 
-    renderItems(basket) {
+    renderItemList(metadata, basket) {
         if(this.state.items) {
             let items = this.state.items
             if(items.length > 0) {
-                basket.getTotalPrice().then((val)=>{
-                    updateBasketTotal(val);
-                })
-
                 return (
                     <div>
                         {
                             items.map((item, key) => (
-                                <BasketItem parent={this} basket={basket} key={key} item={item} />
+                                <BasketItem parent={this} metadata={metadata.filter(i => i._id == item.id)[0]} basket={basket} key={key} item={item} />
                             ))
                         }
                     </div>
@@ -126,40 +128,69 @@ export default class Basket extends React.Component {
         } else return <div>Cargando...</div>
     }
 
-    render() {
-        const {basket} = this.props
+    itemsQuery(environment, basket) {
         return (
-            <div>
-                <Container
-                    style={{textAlign: "center"}}
-                    id={"cart"}
-                    header={[<Link to="/">{"Tienda"}</Link>, " > Carrito "]}
-                >
-                    <BasketHeader />
-                    {this.renderItems(basket)}
-                    <BasketFooter {...this.props} subtotal={basketTotal ? basketTotal : 0} />
-                </Container>
-                <Container
-                    style={{marginLeft: "30px", marginRight: "30px"}}
-                    id={"cart"}
-                    header={[<Link to="/store">{"Tienda"}</Link>, " > Cupones"]}
-                >
-                    <span style={{textAlign: "right", fontSize: "20px"}}>¿Tienes un cupón?</span>
-                    <form className="login-form" style={{width: "100%", marginBottom: "0px"}}>
-                        <p><input id="coupon" type="text" placeholder={"Ejemplo: " + '"CUP0NF4CH3R0"'} style={{width: "100%"}} /></p>
-                        <p>
-                            <input type="submit" value="Canjear"
-                            style={{
-                                fontWeight: "bold",
-                                width: "100%",
-                                cursor: "pointer",
-                                background: "#ffc44d",
-                                color: "#000000"
-                            }} />
-                        </p>
-                    </form>
-                </Container>
-            </div>
+            <QueryRenderer
+                environment={environment}
+                query={fetchAllProductsQuery}
+                render={({ error, props }) => {
+                    if (error) {
+                        return <div>{error.message}</div>
+                    }
+    
+                    if (!props) {
+                        return (
+                            <>
+                            {"Cargando..."}
+                            </>
+                        )
+                    }
+    
+                    const metadata = props.fetchAllProducts;
+                    return (
+                        <div>
+                            <Container
+                                style={{textAlign: "center"}}
+                                id={"cart"}
+                                header={[<Link to="/">{"Tienda"}</Link>, " > Carrito "]}
+                            >
+                                <BasketHeader />
+                                {this.renderItemList(metadata, basket)}
+                                <BasketFooter metadata={metadata} basket={basket} />
+                            </Container>
+                            
+                        </div>
+                    )
+                }}
+            />
+        )
+    }
+
+    render() {
+        return (
+            <>
+            {this.itemsQuery(this.props.environment, this.props.basket)}
+            <Container
+                style={{marginLeft: "30px", marginRight: "30px"}}
+                id={"cart"}
+                header={[<Link to="/store">{"Tienda"}</Link>, " > Cupones"]}
+            >
+                <span style={{textAlign: "right", fontSize: "20px"}}>¿Tienes un cupón?</span>
+                <form className="login-form" style={{width: "100%", marginBottom: "0px"}}>
+                    <p><input id="coupon" type="text" placeholder={"Ejemplo: " + '"CUP0NF4CH3R0"'} style={{width: "100%"}} /></p>
+                    <p>
+                        <input type="submit" value="Canjear"
+                        style={{
+                            fontWeight: "bold",
+                            width: "100%",
+                            cursor: "pointer",
+                            background: "#ffc44d",
+                            color: "#000000"
+                        }} />
+                    </p>
+                </form>
+            </Container>
+            </>
         )
     }
 }
