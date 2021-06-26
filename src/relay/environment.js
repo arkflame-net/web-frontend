02c4185e7119ffcc8c89294e2248ace4c4ab96cc
Valentina Avalos <sammwy.dev@gmail.com>
2021-06-26 20:49:32 -0300

    Added production backend url in relay environment

--- src/relay/environment.js
+++ src/relay/environment.js
@@ -26,7 +26,7 @@ async function fetchQuery(operation, variables = {}, cacheConfig) {
         return fromCache;
     }
 
-    return fetch('http://localhost:4000/graphql', {
+    return fetch('https://api.arkflame.com/graphql', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
